2a9639a4ad3653b9b6f4e6c586b88b8143f008ae
Joshua Jones <joshuabrandonjones@gmail.com>
2021-02-20 22:23:47 -0600

    upload new scripts

--- AdminUser.sh
+++ AdminUser.sh
@@ -1,11 +1,16 @@
-export admin=Unicom
-export pw="We are @ 1011 Hwy6S Suite200! 01"
-dscl . -create /Users/$admin
-dscl . -create /Users/$admin UserShell /bin/bash
-dscl . -create /Users/$admin RealName "$admin"
-dscl . -create /Users/$admin UniqueID "510"
-dscl . -create /Users/$admin PrimaryGroupID 20
-dscl . -create /Users/$admin NFSHomeDirectory /Users/$admin
-dscl . -passwd /Users/$admin $pw
-
-dscl . -append /Groups/admin GroupMembership $admin
+export adminuser=Joshua
+export adminpw=TalkingMrEd2
+export user=Unicom
+export pw="We are @ 5211"
+export fn=Unicom
+export ln=Technologies
+export hint=We are @
+sudo pwpolicy clearaccountpolicies #this may not be needed
+sysadminctl -addUser $user -fullName "$fn $ln" -password $pw -hint $hint
+#sysadminctl interactive -secureTokenOn $user -password - #this is interactive turn on of token
+#sysadminctl interactive -secureTokenOff $user -password - #this is interactive turn off of token
+#sysadminctl -adminUser $adminuser -adminPassword - -secureTokenOn $user -password - #this is interactive turn on of token
+sysadminctl -adminUser $adminuser -adminPassword $adminpw -secureTokenOn $user -password $pw
+sysadminctl -secureTokenOn $user 2>&1 | awk '{print$7}' #testing after enabling
+dscl . -append /Groups/admin GroupMembership $user
+sysadminctl -guestAccount Off
--- symboliclinks.sh
+++ symboliclinks.sh
@@ -1,4 +1,6 @@
 echo enter username
 read username
+mv /users/$username/Desktop /users/$username/Desktop-Old
+mv /users/$username/Documents /users/$username/Documents-Old
 ln -s "/users/$username/OneDrive - Unicom Technologies/Desktop" /users/$username
 ln -s "/users/$username/OneDrive - Unicom Technologies/Documents" /users/$username
