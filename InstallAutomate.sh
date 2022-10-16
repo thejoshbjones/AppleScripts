d62013f83a0e1a5d46158fd59b4662e666c8b88b
Joshua Jones <joshuabrandonjones@gmail.com>
2022-10-16 15:49:08 -0500

    Update InstallAutomate.sh

--- InstallAutomate.sh
+++ InstallAutomate.sh
@@ -2,8 +2,8 @@ export cp=K5IfDHB
 cd $Home
 mkdir ltechagent
 cd $Home\ltechagent
-curl -O https://unicom-tech.hostedrmm.com/share/wkinstall/LTechAgent/config.sh -u unicom-tech4389:$cp
-curl -O https://unicom-tech.hostedrmm.com/share/wkinstall/LTechAgent/LTSvc.mpkg -u unicom-tech4389:$cp
+curl -O https://sub.domainhost.com/share/wkinstall/LTechAgent/config.sh -u adminuser4389:$cp
+curl -O https://sub.domainhost.com/share/wkinstall/LTechAgent/LTSvc.mpkg -u adminuser4389:$cp
 #ls
 #read -p 'test'
 #sudo installer -pkg LTSvc.mpkg -target /
